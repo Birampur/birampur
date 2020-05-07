e41895f5c73977904eff7f590d57455690b0f996
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2020-05-07 17:23:27 +0600

    index3 added

---
+++ leaflet-gsheets.js
@@ -0,0 +1,204 @@
+/* global L Tabletop */
+
+/*
+ * Script to display two tables from Google Sheets as point and polygon layers using Leaflet
+ * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
+ */
+
+// init() is called as soon as the page loads
+function init() {
+  // PASTE YOUR URLs HERE
+  // these URLs come from Google Sheets 'shareable link' form
+  // the first is the polygon layer and the second the points
+  var pointsURL =
+    "https://docs.google.com/spreadsheets/d/12Pzc3r2SvfQvTBFy6JuSBfVzBK714rmzTeI8vO_Hiw8/edit?usp=sharing";
+
+  Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
+}
+window.addEventListener("DOMContentLoaded", init);
+
+
+
+// Create a new Leaflet map centered on the continental US
+var map = L.map("map").setView([25.3921, 88.9546], 13);
+
+// This is the Carto Positron basemap
+var basemap = L.tileLayer(
+  "https://raw.githubusercontent.com/arahmandc/birampur/master/gameimage/{z}/{x}/{y}.png",
+  {
+    attribution:"",
+    subdomains: "abcd",
+    maxZoom: 19,
+    minZoom: 12
+  }
+);
+
+basemap.addTo(map);
+
+
+
+//Scale
+L.control.scale().addTo(map);
+
+
+
+map.zoomControl.setPosition('bottomleft');
+
+
+
+        //bound box
+  var bounds_group = new L.featureGroup([]);
+  function setBounds() {
+  if (bounds_group.getLayers().length) {
+    map.fitBounds(bounds_group.getBounds());
+  }
+  map.setMaxBounds([[25.29,88.83],[25.49,89.09]]);
+}
+setBounds();
+
+// var sidebar = L.control
+//   .sidebar({
+//     container: "sidebar",
+//     closeButton: true,
+//     position: "right"
+//   })
+//   .addTo(map);
+
+// let panelID = "my-info-panel";
+// var panelContent = {
+//   id: panelID,
+//   tab: "<i class='fa fa-bars active'></i>",
+//   pane: "<p id='sidebar-content'></p>",
+//   title: "<h2 id='sidebar-title'>No state selected</h2>"
+// };
+// sidebar.addPanel(panelContent);
+
+
+// These are declared outisde the functions so that the functions can check if they already exist
+
+var pointGroupLayer;
+
+
+
+// addPoints is a bit simpler, as no GeoJSON is needed for the points
+// It does the same check to overwrite the existing points layer once the Google Sheets data comes along
+function addPoints(data) {
+  if (pointGroupLayer != null) {
+    pointGroupLayer.remove();
+  }
+  pointGroupLayer = L.layerGroup().addTo(map);
+
+  // Choose marker type. Options are:
+  // (these are case-sensitive, defaults to marker!)
+  // marker: standard point with an icon
+  // circleMarker: a circle with a radius set in pixels
+  // circle: a circle with a radius set in meters
+  var markerType = "marker";
+
+  // Marker radius
+  // Wil be in pixels for circleMarker, metres for circle
+  // Ignore for point
+  var markerRadius = 100;
+
+  for (var row = 0; row < data.length; row++) {
+    var marker;
+    if (markerType == "circleMarker") {
+      marker = L.circleMarker([data[row].lat, data[row].lon], {radius: markerRadius});
+    } else if (markerType == "circle") {
+      marker = L.circle([data[row].lat, data[row].lon], {radius: markerRadius});
+    } else {
+      marker = L.marker([data[row].lat, data[row].long]);
+    }
+    marker.addTo(pointGroupLayer);
+
+
+    // UNCOMMENT THIS LINE TO USE POPUPS
+    //marker.bindPopup('<h2>' + data[row].location + '</h2>There's a ' + data[row].level + ' ' + data[row].category + ' here');
+
+    // COMMENT THE NEXT 14 LINES TO DISABLE SIDEBAR FOR THE MARKERS
+    marker.feature = {
+      properties: {
+        Name: data[row].Name,
+        lat: data[row].lat,
+        long: data[row].long,
+        category: data[row].category
+      }
+    };
+    marker.on({
+      click: function(e) {
+        L.DomEvent.stopPropagation(e);
+
+        
+        // document.getElementById("sidebar-title").innerHTML =
+        //   e.target.feature.properties.location;
+        // document.getElementById("sidebar-content").innerHTML =
+        //   e.target.feature.properties.category;
+        // sidebar.open(panelID);
+    //      var html =("<h3>"+feature.properties.Name+"</h3>");
+    // layer.bindPopup(html);
+        
+      }
+
+
+    });
+
+
+    // AwesomeMarkers is used to create fancier icons
+    var icon = L.AwesomeMarkers.icon({
+      icon: "info-sign",
+      iconColor: "white",
+      markerColor: getColor(data[row].category),
+      prefix: "glyphicon",
+      extraClasses: "fa-rotate-0"
+    });
+    if (!markerType.includes("circle")) {
+      marker.setIcon(icon);
+    }
+  }
+}
+
+// Returns different colors depending on the string passed
+// Used for the points layer
+function getColor(type) {
+  switch (type) {
+  case "Coffee Shop":
+    return "green";
+  case "Restaurant":
+    return "blue";
+  default:
+    return "green";
+  }
+}
+
+map.addControl( new L.Control.Compass({position: "topright"}) );
+L.control.locate().addTo(map);
+
+ //search options
+      map.addControl(new L.Control.Search({
+            // layer: layer_OsmFile_3,
+            initial: false,
+            hideMarkerOnCollapse: true,
+            propertyName: 'name'}));
+        document.getElementsByClassName('search-button')[0].className +=
+         ' fa fa-binoculars';
+
+
+    // slide menu contents and position
+
+  // var contents = '<h4 style="text-align:center">Birampur Map</h4>';
+  var contents = '<img src="images/bmap.png" alt="BirampurMap" width="300">';
+  contents += '<h5>Mobile-friendly web maps<br>with importent location markers.</h5>';
+  
+  // contents += '<img src="images/BikeMapCodeKey.png" alt="BikeMapCode key" width="196" height="299">';
+  // contents += '<p>Distributed as-is with no warranty</p>';
+  // contents += '<p>Stop your bike in a safe place before using</p>';
+  contents += '<p>Requires internet access to view maps and location</p>';
+  contents += '<p>Allow smartphone browser to show your location<br> Phone > Settings > Privacy > Location Services (On) > While Using<br></p>';
+  contents += '<p>Click OK if browser asks permission to show your location</p>';
+  contents += '<a href="#" ; target:"_blank"> <img src="images/birampurfb.png" alt="Facebook" width="300" height="120"> </a>';
+  // contents += '<h5>Open Cycle Map layer legend</h5>';
+  // contents += '<img src="images/OpenCycleMapKey.png" alt="OpenCycleMap key" width="290" height="438">';
+  var slideMenu = L.control.slideMenu('', {position: 'topright', width: '300px',  height: '100%', delay: '1'}).addTo(map);
+  slideMenu.setContents(contents);
+
+
